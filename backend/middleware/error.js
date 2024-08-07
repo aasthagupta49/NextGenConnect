588e0f358478519ffa0c82cf064fe2fafa1306b6
exclusiveabhi <imexclusiveabhi@gmail.com>
2024-08-07 20:16:08 +0530

    fixes

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -1,35 +1,26 @@
-class errorHandler extends Error{ //yaha pr error ko handle krenge jo backend mai aayengi!
-    constructor(message,statusCode){
-        super(message); //with error message and status code!
-        this.statusCode = statusCode;
-    }
-}
- 
-export const errorMiddleware = (err, req ,res ,next)=>{
- err.message = err.message || "Internal Server Error"  // iska mtlb || agar equal nhi hua toh!
- err.statusCode = err.statusCode || 500
-if(err.name == "CaseError"){
-    const message = `Duplicate at ${Object.keys(err.keyValue)} Entered`
-    err = new errorHandler(message ,400)
-}
-if(err.code == 1100){
-    const message = `Resource not found at ${err.path}`
-    err = new errorHandler(message ,400)
-}
+import errorHandler from './errorHandler';
 
-if(err.name == "JsonWebTokenError"){
-    const message = `Json web token is invalid`
-    err = new errorHandler(message ,400)
-}
+export const errorMiddleware = (err, req, res, next) => {
+  err.message = err.message || "Internal Server Error";
+  err.statusCode = err.statusCode || 500;
 
-if(err.name == "TokenExpiredError"){
-    const message = `Token is Expired, Try Again`
-    err = new errorHandler(message ,400)
-}
-return res.status(statusCode).json({
-    sucess: false,
+  // Handle specific errors
+  if (err.name === "CastError") {
+    const message = `Invalid ${err.path}: ${err.value}`;
+    err = new errorHandler(message, 400);
+  } else if (err.code === 11000) {
+    const message = `Duplicate field value entered`;
+    err = new errorHandler(message, 400);
+  } else if (err.name === "JsonWebTokenError") {
+    const message = "Json web token is invalid";
+    err = new errorHandler(message, 401); // Typically 401 for unauthorized
+  } else if (err.name === "TokenExpiredError") {
+    const message = "Token is expired, please log in again";
+    err = new errorHandler(message, 401); // Typically 401 for unauthorized
+  }
+
+  res.status(err.statusCode).json({
+    success: false,
     message: err.message
-})
-
-}
-export default errorHandler;
+  });
+};
